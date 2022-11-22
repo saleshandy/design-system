ad47eaca23465f461a8eee42bd0caf89e3c94546
Shoaib Sayyed <shoaib@saleshandy.com>
2022-11-22 13:20:17 +0530

    fix: fixed import component issues

--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
-import { Button, ButtonProps } from '../components/button';
+import { Button, ButtonProps } from '../';
 
 export default {
   title: 'Components/Button',
