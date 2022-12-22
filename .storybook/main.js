1ba05977c251e0f5d7a1e21445dbf50b9f65a557
Shoaib Sayyed <shoaib@saleshandy.com>
2022-12-22 15:59:03 +0530

    feat(SH-4359): added button component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/**/*.stories.@(mdx)',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -7,5 +10,6 @@ module.exports = {
     '@storybook/addon-a11y',
     '@storybook/preset-scss',
     'storybook-addon-sass-postcss',
+    'storybook-addon-designs',
   ],
 };
