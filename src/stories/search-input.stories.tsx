22cabf6ee334606fb69da1f5aac06b9d5de9bd54
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2024-02-14 05:06:45 +0000

    feat: added search component

---
+++ src/stories/search-input.stories.tsx
@@ -0,0 +1,31 @@
+import { StoryFn, Meta } from '@storybook/react';
+import { SearchInput, SearchInputProps } from '../components/search-input';
+
+export default {
+  title: 'Components/SearchInput',
+  component: SearchInput,
+  argTypes: {
+    placeholder: {
+      description: 'palceholder for search-input',
+    },
+    variant: {
+      control: 'select',
+      options: ['bg-light', 'bg-gray'], 
+      description: 'Background variant of the search input',
+    },
+  },
+} as Meta;
+
+const Template: StoryFn<SearchInputProps> = (args) => <SearchInput {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  variant: 'bg-light',
+};
+
+export const BgGray = Template.bind({});
+
+BgGray.args = {
+  variant: 'bg-gray',
+};
