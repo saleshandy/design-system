822e050ccbf1208941e240bffd898d0b1e37973c
vatsal-prajapati-2002 <vatsal.prajapati@saleshandy.com>
2024-07-01 16:14:18 +0530

    add default value to description

--- src/stories/alert.stories.tsx
+++ src/stories/alert.stories.tsx
@@ -13,15 +13,37 @@ Template.args = {
   header: '',
   variant: 'info',
   description: '',
+  header: '',
   size: 'lg',
 };
 
 export const Variants: StoryFn<AlertProps> = () => (
   <div className="alert-story__container">
-    <Alert variant="info" description=""></Alert>
-    <Alert variant="alert" description=""></Alert>
-    <Alert variant="warning" description=""></Alert>
-    <Alert variant="tricks" description=""></Alert>
-    <Alert variant="success" description=""></Alert>
+    <Alert
+      variant="info"
+      description="All prospects matching domains/emails from this list will be blacklisted in all sequences (including client’s sequences) and will not be contacted further."
+    ></Alert>
+    <Alert
+      variant="info"
+      header="Domain setup is in progress!"
+      description="It can take up to 48 hours. Explore the product in the meantime, and we'll notify you when your brand domain is ready."
+    ></Alert>
+    <Alert
+      variant="alert"
+      description="All prospects matching domains/emails from this list will be blacklisted in all sequences (including client’s sequences) and will not be contacted further."
+    ></Alert>
+    <Alert
+      variant="warning"
+      description="All prospects matching domains/emails from this list will be blacklisted in all sequences (including client’s sequences) and will not be contacted further."
+    ></Alert>
+    <Alert
+      variant="tricks"
+      description="All prospects matching domains/emails from this list will be blacklisted in all sequences (including client’s sequences) and will not be contacted further."
+    ></Alert>
+    <Alert
+      variant="success"
+      header="Domain setup is in progress!"
+      description="All set! Login with your brand domain now. Click here"
+    ></Alert>
   </div>
 );
